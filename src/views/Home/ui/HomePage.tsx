aea462e8e9fb2a56410bf295013458e5a91cb1c1
Rikcher <nick.richardson.4884@gmail.com>
2026-04-04 15:14:42 +0700

    feat: setup home page

---
+++ src/views/Home/ui/HomePage.tsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+  return (
+    <main className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      test
+    </main>
+  );
+};
